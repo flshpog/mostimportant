cabc31f30d9ce6170789a50645745e9849c852c2
fishpog <126018624+flshpog@users.noreply.github.com>
2026-09-09 12:43:43 -0400

    Update application-commands.js

--- src/commands/util/application-commands.js
+++ src/commands/util/application-commands.js
@@ -30,8 +30,8 @@ const WELCOME = {
 // fires on `!recruit-1`. Paste each step's payload as you provide it.
 const steps = {
     "recruit-1": {
-        "username": "Orville",
-        "avatar_url": "https://i.ytimg.com/vi/hyzqLZri-E0/oar2.jpg?sqp=-oaymwEYCJUDENAFSFqQAgHyq4qpAwcIARUAAIhC&rs=AOn4CLByKJA0CJ_ehgG6a7T2fNIQBDA5BQ&usqp=CCk",
+        "username": "Super Mario",
+        "avatar_url": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/69d79c77-7a14-4d6e-a6e4-6aadb16f4fdb/dfyts5a-befb4a6c-e651-4361-91c0-ac23a65905d9.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiIvZi82OWQ3OWM3Ny03YTE0LTRkNmUtYTZlNC02YWFkYjE2ZjRmZGIvZGZ5dHM1YS1iZWZiNGE2Yy1lNjUxLTQzNjEtOTFjMC1hYzIzYTY1OTA1ZDkuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.9vYZfwklBp3Nk6SVhJkjus1HBhKv514jp84LOFkuJD0",
         "embeds": [
             {
                 "title": "Recruit 1",
@@ -44,9 +44,8 @@ const steps = {
     },
 
     "recruit-2": {
-        "username": "Orville",
-        "avatar_url": "https://i.ytimg.com/vi/hyzqLZri-E0/oar2.jpg?sqp=-oaymwEYCJUDENAFSFqQAgHyq4qpAwcIARUAAIhC&rs=AOn4CLByKJA0CJ_ehgG6a7T2fNIQBDA5BQ&usqp=CCk",
-        "embeds": [
+        "username": "Super Mario",
+        "avatar_url": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/69d79c77-7a14-4d6e-a6e4-6aadb16f4fdb/dfyts5a-befb4a6c-e651-4361-91c0-ac23a65905d9.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiIvZi82OWQ3OWM3Ny03YTE0LTRkNmUtYTZlNC02YWFkYjE2ZjRmZGIvZGZ5dHM1YS1iZWZiNGE2Yy1lNjUxLTQzNjEtOTFjMC1hYzIzYTY1OTA1ZDkuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.9vYZfwklBp3Nk6SVhJkjus1HBhKv514jp84LOFkuJD0",        "embeds": [
             {
                 "title": "Recruit 2",
                 "description": "Let’s get a little deeper into it. Of course, we aren’t going to make you do a standard interview. We just have proper question for you today.\n\n**Why will you win Everest Survivor S3: The Mushroom Kingdom?**\n\nWhen you’re ready to continue, you can do **`!recruit-3`**.\n",
@@ -58,9 +57,8 @@ const steps = {
     },
 
     "recruit-3": {
-        "username": "Orville",
-        "avatar_url": "https://i.ytimg.com/vi/hyzqLZri-E0/oar2.jpg?sqp=-oaymwEYCJUDENAFSFqQAgHyq4qpAwcIARUAAIhC&rs=AOn4CLByKJA0CJ_ehgG6a7T2fNIQBDA5BQ&usqp=CCk",
-        "embeds": [
+        "username": "Super Mario",
+        "avatar_url": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/69d79c77-7a14-4d6e-a6e4-6aadb16f4fdb/dfyts5a-befb4a6c-e651-4361-91c0-ac23a65905d9.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiIvZi82OWQ3OWM3Ny03YTE0LTRkNmUtYTZlNC02YWFkYjE2ZjRmZGIvZGZ5dHM1YS1iZWZiNGE2Yy1lNjUxLTQzNjEtOTFjMC1hYzIzYTY1OTA1ZDkuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.9vYZfwklBp3Nk6SVhJkjus1HBhKv514jp84LOFkuJD0",        "embeds": [
             {
                 "title": "Recruit 3",
                 "description": "And now for the fun part!\n\nThis season, each castaway will be represented by a **villager**. If you’re unfamiliar with Animal Crossing, there are over 400 villagers that can choose to come live on or visit your island.\n\nHead over to [this link](https://animalcrossing.fandom.com/wiki/Villager_list_(New_Horizons)) and find **three** villagers you would like to represent you. Then, send them here in order of highest to lowest priority.\n\nWe work on a first come, first serve basis, so the quicker you do this the higher your chances are of getting your favorite!\n\nThe following villagers are **off-limits**:\n> * Erik (fishpog)\n> * Lopez (Kebab)\n> * Raymond (Mike)\n> * Fang (Fio)\n> * Admiral (xSoul)\n> * Sasha (Lemmy)\n> * Kid Cat (Woofley)\n> * Boots (Void)\n> * Freya (Fuggles)\n> - Any **special** characters, i.e. Tom Nook or Blathers\n\nTo continue, do **`!acknowledgements`**.",
@@ -72,9 +70,8 @@ const steps = {
     },
 
     "acknowledgements": {
-        "username": "Orville",
-        "avatar_url": "https://i.ytimg.com/vi/hyzqLZri-E0/oar2.jpg?sqp=-oaymwEYCJUDENAFSFqQAgHyq4qpAwcIARUAAIhC&rs=AOn4CLByKJA0CJ_ehgG6a7T2fNIQBDA5BQ&usqp=CCk",
-        "embeds": [
+        "username": "Super Mario",
+        "avatar_url": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/69d79c77-7a14-4d6e-a6e4-6aadb16f4fdb/dfyts5a-befb4a6c-e651-4361-91c0-ac23a65905d9.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiIvZi82OWQ3OWM3Ny03YTE0LTRkNmUtYTZlNC02YWFkYjE2ZjRmZGIvZGZ5dHM1YS1iZWZiNGE2Yy1lNjUxLTQzNjEtOTFjMC1hYzIzYTY1OTA1ZDkuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.9vYZfwklBp3Nk6SVhJkjus1HBhKv514jp84LOFkuJD0",        "embeds": [
             {
                 "description": "# ACKNOWLEDGEMENTS & UNDERSTANDINGS\n\nIn order for you to formally enter the casting pool, you must read and agree to all of the following:\n\n1. *I have read the <#1414321682839109818> and understand what I can be given a strike for. I recognize that cheating of any kind and extreme instances of toxicity will not be tolerated in this environment.*\n\n2. *I am signing up for a game that can be very mentally taxing at times. While this ORG is important, I understand that real life and mental health will always take priority over a silly online game.*\n\n3. *It is expected of me to do confessionals each episode in my confessional channel. I will not leave my confessional blank and actively contribute content (and my personal narrative) to the season!*\n\n4. *I understand that this will be a time commitment, and I will do my best to be available as often as I can be. While it is not expected of me to be on Discord 24/7, I have the free time needed to be an active participant in the game.*\n\nIf you don't want to fill out a DNC list, you're done! Continue with **`!finish`**.\nIf you DO feel the need to fill one out, the command to do so is **`!dnc-list`**.",
                 "image": {
@@ -85,9 +82,8 @@ const steps = {
     },
 
     "dnc-list": {
-        "username": "Orville",
-        "avatar_url": "https://i.ytimg.com/vi/hyzqLZri-E0/oar2.jpg?sqp=-oaymwEYCJUDENAFSFqQAgHyq4qpAwcIARUAAIhC&rs=AOn4CLByKJA0CJ_ehgG6a7T2fNIQBDA5BQ&usqp=CCk",
-        "embeds": [
+        "username": "Super Mario",
+        "avatar_url": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/69d79c77-7a14-4d6e-a6e4-6aadb16f4fdb/dfyts5a-befb4a6c-e651-4361-91c0-ac23a65905d9.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiIvZi82OWQ3OWM3Ny03YTE0LTRkNmUtYTZlNC02YWFkYjE2ZjRmZGIvZGZ5dHM1YS1iZWZiNGE2Yy1lNjUxLTQzNjEtOTFjMC1hYzIzYTY1OTA1ZDkuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.9vYZfwklBp3Nk6SVhJkjus1HBhKv514jp84LOFkuJD0",        "embeds": [
             {
                 "description": "# DNC INFO\nLet's be very clear. **I do not like or endorse cancel culture, and I especially do not like a mob mentality against one particular person**. That being said, at Everest, we want to ensure that every player feels comfortable within the game. So with that being said, here is how this will work.\n\nIf you have someone who you have PERSONAL beef with or someone who you just absolutely cannot stand for one reason or another, please let us know their name and discord tag. We can guarantee that you will not start on the same Tribe to minimize the impact of your feud/prevent metagaming.\n\nIf there is someone who you OBJECTIVELY believe should not be allowed to touch this season with a 20-foot pole, please let us know their name, discord tag and a list of reasons they should not play the season. We 100% expect you to provide evidence (screenshots, docs, whatever) to back up your claims here.\n\nAfter *this*, you're done. do **`!finish`** to finish your interview.",
                 "image": {
@@ -98,9 +94,8 @@ const steps = {
     },
 
     "finish": {
-        "username": "Orville",
-        "avatar_url": "https://i.ytimg.com/vi/hyzqLZri-E0/oar2.jpg?sqp=-oaymwEYCJUDENAFSFqQAgHyq4qpAwcIARUAAIhC&rs=AOn4CLByKJA0CJ_ehgG6a7T2fNIQBDA5BQ&usqp=CCk",
-        "embeds": [
+        "username": "Super Mario",
+        "avatar_url": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/69d79c77-7a14-4d6e-a6e4-6aadb16f4fdb/dfyts5a-befb4a6c-e651-4361-91c0-ac23a65905d9.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiIvZi82OWQ3OWM3Ny03YTE0LTRkNmUtYTZlNC02YWFkYjE2ZjRmZGIvZGZ5dHM1YS1iZWZiNGE2Yy1lNjUxLTQzNjEtOTFjMC1hYzIzYTY1OTA1ZDkuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.9vYZfwklBp3Nk6SVhJkjus1HBhKv514jp84LOFkuJD0",        "embeds": [
             {
                 "description": "# ...And that's all!\n\nThank you again for your interest in playing.\n\nWe are extremely excited for this season, and we hope you are equally as excited to play.\n\nIf you wish, ping Producers to let us know you've completed your interview. If not, Fio would probably like you just a little bit more.",
                 "image": {
@@ -111,9 +106,8 @@ const steps = {
     },
 
     "interview-1": {
-        "username": "Orville",
-        "avatar_url": "https://i.ytimg.com/vi/hyzqLZri-E0/oar2.jpg?sqp=-oaymwEYCJUDENAFSFqQAgHyq4qpAwcIARUAAIhC&rs=AOn4CLByKJA0CJ_ehgG6a7T2fNIQBDA5BQ&usqp=CCk",
-        "embeds": [
+        "username": "Super Mario",
+        "avatar_url": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/69d79c77-7a14-4d6e-a6e4-6aadb16f4fdb/dfyts5a-befb4a6c-e651-4361-91c0-ac23a65905d9.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiIvZi82OWQ3OWM3Ny03YTE0LTRkNmUtYTZlNC02YWFkYjE2ZjRmZGIvZGZ5dHM1YS1iZWZiNGE2Yy1lNjUxLTQzNjEtOTFjMC1hYzIzYTY1OTA1ZDkuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.9vYZfwklBp3Nk6SVhJkjus1HBhKv514jp84LOFkuJD0",        "embeds": [
             {
                 "title": "Interview 1",
                 "description": "Let's begin by making sure we have everything right for our **cast info**.\n\nPlease provide..\n> * Your **name**\n> * Your **age**\n> * Your **pronouns**\n> * Your **timezone**\n\nTo continue, **`!interview-2`**.",
@@ -125,9 +119,8 @@ const steps = {
     },
 
     "interview-2": {
-        "username": "Orville",
-        "avatar_url": "https://i.ytimg.com/vi/hyzqLZri-E0/oar2.jpg?sqp=-oaymwEYCJUDENAFSFqQAgHyq4qpAwcIARUAAIhC&rs=AOn4CLByKJA0CJ_ehgG6a7T2fNIQBDA5BQ&usqp=CCk",
-        "embeds": [
+        "username": "Super Mario",
+        "avatar_url": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/69d79c77-7a14-4d6e-a6e4-6aadb16f4fdb/dfyts5a-befb4a6c-e651-4361-91c0-ac23a65905d9.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiIvZi82OWQ3OWM3Ny03YTE0LTRkNmUtYTZlNC02YWFkYjE2ZjRmZGIvZGZ5dHM1YS1iZWZiNGE2Yy1lNjUxLTQzNjEtOTFjMC1hYzIzYTY1OTA1ZDkuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.9vYZfwklBp3Nk6SVhJkjus1HBhKv514jp84LOFkuJD0",        "embeds": [
             {
                 "title": "Interview 2",
                 "description": "*Now let's get into this.*\n\n*What's your reasoning for applying to this season? Are you interested in the theme? Were you in and around season 1 of Everest and wanted to take a crack at season 2? Let us know!*\n\nTo continue, **`!interview-3`**.",
@@ -139,9 +132,8 @@ const steps = {
     },
 
     "interview-3": {
-        "username": "Orville",
-        "avatar_url": "https://i.ytimg.com/vi/hyzqLZri-E0/oar2.jpg?sqp=-oaymwEYCJUDENAFSFqQAgHyq4qpAwcIARUAAIhC&rs=AOn4CLByKJA0CJ_ehgG6a7T2fNIQBDA5BQ&usqp=CCk",
-        "embeds": [
+        "username": "Super Mario",
+        "avatar_url": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/69d79c77-7a14-4d6e-a6e4-6aadb16f4fdb/dfyts5a-befb4a6c-e651-4361-91c0-ac23a65905d9.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiIvZi82OWQ3OWM3Ny03YTE0LTRkNmUtYTZlNC02YWFkYjE2ZjRmZGIvZGZ5dHM1YS1iZWZiNGE2Yy1lNjUxLTQzNjEtOTFjMC1hYzIzYTY1OTA1ZDkuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.9vYZfwklBp3Nk6SVhJkjus1HBhKv514jp84LOFkuJD0",        "embeds": [
             {
                 "title": "Interview 3",
                 "description": "Now into the personal territory...\n\n*Tell us about your ORG history! Are you the kind of player who's always involved in a game? Are you more of a once-a-year kind of player? Did you come out of retirement just for this? Please share your experiences with any fun or notable games from your ORG career!*\n\n*...or if this is your first ORG ever, welcome! Are you a fan of the show, did you get introduced by a friend? How'd you end up here? More importantly... how do you plan to navigate the game for your first time playing?*\n\n*Additionally, what from your previous ORG runs do you want to replicate here in Everest? What worked well for you before? What do you want to change this time around? Give us the tea!*\n\nTo continue, **`!interview-4`**.",
@@ -153,9 +145,8 @@ const steps = {
     },
 
     "interview-4": {
-        "username": "Orville",
-        "avatar_url": "https://i.ytimg.com/vi/hyzqLZri-E0/oar2.jpg?sqp=-oaymwEYCJUDENAFSFqQAgHyq4qpAwcIARUAAIhC&rs=AOn4CLByKJA0CJ_ehgG6a7T2fNIQBDA5BQ&usqp=CCk",
-        "embeds": [
+        "username": "Super Mario",
+        "avatar_url": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/69d79c77-7a14-4d6e-a6e4-6aadb16f4fdb/dfyts5a-befb4a6c-e651-4361-91c0-ac23a65905d9.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiIvZi82OWQ3OWM3Ny03YTE0LTRkNmUtYTZlNC02YWFkYjE2ZjRmZGIvZGZ5dHM1YS1iZWZiNGE2Yy1lNjUxLTQzNjEtOTFjMC1hYzIzYTY1OTA1ZDkuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.9vYZfwklBp3Nk6SVhJkjus1HBhKv514jp84LOFkuJD0",        "embeds": [
             {
                 "title": "Interview 4",
                 "description": "We've got just 2 more questions for you today.\n\n*Every Survivor season is full of characters… heroes, villains, side-kicks, comedic reliefs, trainwrecks and so much more. How would you describe the kind of character you anticipate being this season?*\n\n*Enough about Survivor and ORGs now. Tell us a little more about yourself, outside of the game. How do you describe yourself, and how might that compare to how OTHERS describe you? Additionally, tell us about your hobbies and what you do for work/school!*\n\nTo continue, **`!interview-5`**.",
@@ -167,9 +158,8 @@ const steps = {
     },
 
     "interview-5": {
-        "username": "Orville",
-        "avatar_url": "https://i.ytimg.com/vi/hyzqLZri-E0/oar2.jpg?sqp=-oaymwEYCJUDENAFSFqQAgHyq4qpAwcIARUAAIhC&rs=AOn4CLByKJA0CJ_ehgG6a7T2fNIQBDA5BQ&usqp=CCk",
-        "embeds": [
+        "username": "Super Mario",
+        "avatar_url": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/69d79c77-7a14-4d6e-a6e4-6aadb16f4fdb/dfyts5a-befb4a6c-e651-4361-91c0-ac23a65905d9.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiIvZi82OWQ3OWM3Ny03YTE0LTRkNmUtYTZlNC02YWFkYjE2ZjRmZGIvZGZ5dHM1YS1iZWZiNGE2Yy1lNjUxLTQzNjEtOTFjMC1hYzIzYTY1OTA1ZDkuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.9vYZfwklBp3Nk6SVhJkjus1HBhKv514jp84LOFkuJD0",        "embeds": [
             {
                 "title": "Interview 5",
                 "description": "And now for the fun part!\n\nThis season, each castaway will be represented by a **villager**. If you’re unfamiliar with Animal Crossing, there are over 400 villagers that can choose to come live on or visit your island.\n\nHead over to [this link](https://animalcrossing.fandom.com/wiki/Villager_list_(New_Horizons)) and find **three** villagers you would like to represent you. Then, send them here in order of highest to lowest priority.\n\nWe work on a first come, first serve basis, so the quicker you do this the higher your chances are of getting your favorite!\n\nThe following villagers are **off-limits**:\n> * Erik (fishpog)\n> * Lopez (Kebab)\n> * Raymond (Mike)\n> * Fang (Fio)\n> * Admiral (xSoul)\n> * Sasha (Lemmy)\n> * Kid Cat (Woofley)\n> * Boots (Void)\n> * Freya (Fuggles)\n> - Any **special** characters, i.e. Tom Nook or Blathers\n\nTo continue, do **`!acknowledgements`**.",
